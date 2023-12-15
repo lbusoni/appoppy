4e894a6f080595beddab35a99f2c018305cc14ba
Lorenzo Busoni <lbusoni@gmail.com>
2023-12-15 22:58:43 +0100

    main with passata residual none

--- appoppy/mains/main231214_1650_mcao.py
+++ appoppy/mains/main231214_1650_mcao.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 
 ROOT_DIR = '/Users/lbusoni/Library/CloudStorage/GoogleDrive-lorenzo.busoni@inaf.it/Il mio Drive/adopt/Varie/CiaoCiaoWFS/analysis/data/from_appoppy/'
 
+TN_NONE = 'none'
 TN_MCAO_1 = '20231209_202232.0_coo55.0_0.0'
 TN_SCAO_1000 = '20231212_212912.0_coo0.0_0.0'
 TN_SCAO_2000 = '20231213_101833.0_coo0.0_0.0'
@@ -31,13 +32,24 @@ def _create_long_exposure_generic(tn,
                                   wavelength=1650*u.nm,
                                   n_iter=1000):
     le = LongExposurePetalometer(
-        tracking_number=_tracknum_code(tn, code), rot_angle=rot_angle,
+        _tracknum_code(tn, code),
+        tn,
+        rot_angle=rot_angle,
         petals=petals,
         wavelength=wavelength,
         n_iter=n_iter)
     le.run()
     le.save(_filename(tn, code))
     return le
+
+
+def create_long_exposure_none_0000():
+    return _create_long_exposure_generic(TN_NONE, '0000', n_iter=100)
+
+
+def create_long_exposure_none_0002():
+    return _create_long_exposure_generic(TN_NONE, '0002', n_iter=100,
+                                         petals=np.array([400, 0, 0, 0, 0, 0]) * u.nm)
 
 
 def create_long_exposure_mcao_0000():
@@ -74,6 +86,14 @@ def create_long_exposure_scao_2000_0001():
 
 def create_long_exposure_scao_2000_0002():
     return _create_long_exposure_generic(TN_SCAO_2000, '0002', petals=np.array([400, 0, 0, 0, 0, 0]) * u.nm)
+
+
+def analyze_none_0000():
+    return _analyze_long_exposure(TN_NONE, '0000')
+
+
+def analyze_none_0002():
+    return _analyze_long_exposure(TN_NONE, '0002')
 
 
 def analyze_mcao_0000():
