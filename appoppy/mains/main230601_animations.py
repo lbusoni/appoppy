5f75d04aab91fcd7bebf3ca5a94ab90df6382b2d
g-carla <giulia.carl9@gmail.com>
2024-01-25 16:00:03 +0100

    Add wind speed parameter in animations of LWE

--- appoppy/mains/main230601_animations.py
+++ appoppy/mains/main230601_animations.py
@@ -2,9 +2,9 @@ from appoppy.gif_animator import Gif2DMapsAnimator
 from appoppy.low_wind_effect import LowWindEffectWavefront
 
 
-def main230601_animate_LWE(vminmax=[0, 5]):
-    root_folder = '/Users/giuliacarla/Documents/INAF/Lavoro/Progetti/MORFEO/Petalometer/CiaoCiaoWFS/analysis/animations/LWE'
-    lwe = LowWindEffectWavefront(wind_speed=0.5)
+def main230601_animate_LWE(vminmax=[0, 5], wind_speed=0.5):
+    root_folder = '/Users/giuliacarla/Documents/INAF/Lavoro/Progetti/MORFEO/Petalometer/CiaoCiaoWFS/analysis/animations/LWE_' + str(wind_speed)
+    lwe = LowWindEffectWavefront(wind_speed=wind_speed)
     lwe_cube_maps = lwe.phase_screens()
     # lwe_cube_maps_rot = rotate(lwe_cube_maps, 30, axes=(1, 2), reshape=False)
     gf = Gif2DMapsAnimator(root_folder, lwe_cube_maps,
